3b6b1d68ab81edef887b13dc8c5a3bf5ea3f7f51
Yan Zarytovsky <yan.zarytovsky@gmail.com>
2020-10-25 00:06:31 +0300

    Add the solution of the exercise 4

--- src/ru/mirea/Main.java
+++ src/ru/mirea/Main.java
@@ -3,6 +3,13 @@ package ru.mirea;
 public class Main {
 
     public static void main(String[] args) {
+        MovableCircle a = new MovableCircle(0, 0, 2, 1, 4);
 
+        System.out.println(a);
+
+        a.moveLeft();
+        a.moveUp();
+
+        System.out.println(a);
     }
 }
---
+++ src/ru/mirea/Movable.java
@@ -0,0 +1,11 @@
+package ru.mirea;
+
+public interface Movable {
+    void moveUp();
+
+    void moveDown();
+
+    void moveLeft();
+
+    void moveRight();
+}
